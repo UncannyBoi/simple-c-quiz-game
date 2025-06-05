0f2869941774c1eb7a4d22e657127edf9f4a4920
Lukas <82063038+UncannyBoi@users.noreply.github.com>
2025-06-06 01:10:36 +0800

    Add files via upload

---
+++ include/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED_CHECK
+#define MAIN_H_INCLUDED_CHECK
+
+
+/* Keep stuff inside IF */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+
+#endif
